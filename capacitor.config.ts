c6ef774dfe52a73fcfc055a35efcf1f15fe29c22
MarjanMaci <marjanmarjanovski13@gmail.com>
2023-04-14 16:24:20 +0200

    iOS app name, icons, splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   "appId": 'com.easyreserve.test',
-  "appName": 'TheBarberApp',
+  "appName": 'MStyling',
   "webDir": 'www',
   "bundledWebRuntime": false,
   "plugins": {
